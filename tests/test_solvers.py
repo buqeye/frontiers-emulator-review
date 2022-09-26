d59e05444521531f56fba22680f89aaefa02d867
Jordan Melendez <jmelendez1992@gmail.com>
2022-09-26 12:51:55 -0400

    More tests

--- tests/test_solvers.py
+++ tests/test_solvers.py
@@ -1,12 +1,89 @@
 import pytest
 
+import numpy as np
+from scipy import stats
+from scipy.special import spherical_jn
+
+from emulate import CompoundMesh
+from emulate import fourier_transform_partial_wave
+from emulate import gaussian_radial_fourier_transform
+from emulate.utils import (
+    yamaguchi_form_factor_momentum_space,
+    yamaguchi_form_factor_position_space,
+    yamaguchi_radial_wave_function,
+    yamaguchi_scattering_amplitude,
+    schrodinger_residual,
+)
+from emulate import (
+    NewtonEmulator,
+    BoundaryCondition,
+    KohnLippmannSchwingerEmulator,
+    SeparableKohnEmulator,
+)
+
 
 def test_lippmann_schwinger_solver():
     pass
 
 
 def test_separable_potential_solver():
-    pass
+    # Rule: The analytic Yamaguchi scattering amplitude should match the empirical version from the LS equation
+
+    # Given a quadrature mesh
+    n_intervals = 21
+    nodes = np.linspace(0, 10, n_intervals)
+    n_points = 100 * np.ones(n_intervals - 1, dtype=int)
+    mesh = CompoundMesh(nodes, n_points)
+    k, dk = mesh.x, mesh.w
+    r, dr = mesh.x, mesh.w
+
+    # And other parameters
+    beta = 2.0
+    ell = 0
+    q_cm = np.array([0.1, 1, 2])
+    strength = np.array([5])
+    hbarsq_over_M = 1
+
+    # And a Yamaguchi potential
+    f_k = yamaguchi_form_factor_momentum_space(k=k, beta=beta, ell=ell)
+    f_r = yamaguchi_form_factor_position_space(r=r, beta=beta, ell=ell)
+    V1_k = (f_k[:, None] * f_k)[..., None]  # Add parameter dimension
+    V1_r = (f_r[:, None] * f_r)[..., None]
+
+    # When the Lippmann Schwidnger equation is evaluated to solve for the reactance matrix, K
+    newton = NewtonEmulator(
+        V0=np.zeros_like(V1_k[..., 0]),
+        V1=V1_k,
+        k=k,
+        dk=dk,
+        q_cm=q_cm,
+        boundary_condition=BoundaryCondition.STANDING,
+        nugget=1e-10,
+    )
+
+    separable = SeparableKohnEmulator(
+        v_r=[f_r],
+        r=r,
+        dr=dr,
+        v_k=[f_k],
+        k=k,
+        dk=dk,
+        q_cm=q_cm,
+        inv_mass=hbarsq_over_M,
+        ell=ell,
+        nugget=1e-7,
+    )
+
+    p = strength * np.array([1])
+    K_half_schwinger = newton.reactance(
+        p=p, include_q=False, shell="half", return_gradient=False
+    )
+    K_half_separable = separable.compute_half_on_shell_reactance(p, include_q=False)
+
+    # Then the K matrix from the LS equation and the analytic version should match
+    np.testing.assert_allclose(
+        actual=K_half_separable, desired=K_half_schwinger, atol=1e-5, rtol=1e-1
+    )
 
 
 def test_newton_emulator():
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,17 +1,24 @@
-from re import I
+# Try to write clean tests following BDD and Gherkin keywords:
+# https://cucumber.io/docs/gherkin/reference/
+
+# from re import I
 import pytest
 
 import numpy as np
 from scipy import stats
+from scipy.special import spherical_jn
+
 from emulate import CompoundMesh
 from emulate import fourier_transform_partial_wave
 from emulate import gaussian_radial_fourier_transform
 from emulate.utils import (
     yamaguchi_form_factor_momentum_space,
     yamaguchi_form_factor_position_space,
+    yamaguchi_radial_wave_function,
     yamaguchi_scattering_amplitude,
+    schrodinger_residual,
 )
-from emulate import NewtonEmulator, BoundaryCondition
+from emulate import NewtonEmulator, BoundaryCondition, KohnLippmannSchwingerEmulator
 
 
 def test_fourier_transform():
@@ -63,15 +70,20 @@ def test_yamaguchi_position_form_factor():
 
 
 def test_yamaguchi_scattering_amplitude():
+    # Rule: The analytic Yamaguchi scattering amplitude should match the empirical version from the LS equation
+
+    # Given a quadrature mesh and other parameters
     mesh = CompoundMesh([0, 0.1, 5, 10], [50, 50, 50, 50])
     k, dk = mesh.x, mesh.w
     beta = 2.0
     q_cm = np.array([0.1, 1, 2])
     strength = np.array([3])
 
+    # And a Yamaguchi potential
     f_k = yamaguchi_form_factor_momentum_space(k=k, beta=beta, ell=0)
     V1 = (f_k[:, None] * f_k)[..., None]
-    # Use emulator class as exact Lippmann Schwinger equation solver
+
+    # When an empirical on-shell K matrix is computed
     newton = NewtonEmulator(
         V0=np.zeros_like(V1[..., 0]),
         V1=V1,
@@ -81,14 +93,139 @@ def test_yamaguchi_scattering_amplitude():
         boundary_condition=BoundaryCondition.STANDING,
         nugget=1e-10,
     )
-
-    # Compute exact K matrix
     K_ls = newton.predict(strength, full_space=True)
-    # Compute K with the analytic function we want to test
+
+    # And the analytic Yamaguchi K matrix is evaluated
     K_analytic = yamaguchi_scattering_amplitude(
         q_cm=q_cm, beta=beta, strength=strength, include_q=True
     )
+
+    # Then these two approaches should match
     np.testing.assert_allclose(actual=K_analytic, desired=K_ls, atol=1e-10, rtol=1e-1)
+
+
+@pytest.mark.parametrize("ell", [0, 1, 2, 3])
+def test_free_wave_function(ell):
+    # Rule: The free wave function should satisfy the Schrodinger equation
+
+    # Given a free wave function on a quadrature mesh in position space
+    # And a choice of angular momentum, ell
+    n_intervals = 21
+    mesh = CompoundMesh(
+        np.linspace(0, 10, n_intervals), 100 * np.ones(n_intervals - 1, dtype=int)
+    )
+    r, dr = mesh.x, mesh.w
+    n_r = len(r)
+    q_cm = np.array([0.1, 1, 2])
+    j_ell = spherical_jn(ell, r * q_cm[:, None])
+
+    # When it is inserted in the Schrodinger equation with no external potential
+    V = np.zeros((n_r, n_r))
+    residual = schrodinger_residual(
+        psi=j_ell, V=V, r=r, dr=dr, q_cm=q_cm[:, None], ell=ell
+    )
+    # And endpoint effects are removed (due to numerical gradients)
+    residual = residual[..., 2:-2]
+
+    # Then the residual should be almost zero
+    np.testing.assert_allclose(
+        actual=residual, desired=np.zeros_like(residual), atol=1e-2, rtol=1
+    )
+
+
+@pytest.mark.parametrize("ell", [0])
+def test_yamaguchi_wave_function(ell):
+    # Rule: The Yamaguchi wave function should satisfy the Schrodinger equation
+
+    # Given a Yamaguchi wave function on a quadrature mesh in position space
+    # And a choice of angular momentum, ell
+    n_intervals = 21
+    mesh = CompoundMesh(
+        np.linspace(0, 10, n_intervals), 100 * np.ones(n_intervals - 1, dtype=int)
+    )
+    r, dr = mesh.x, mesh.w
+    # n_r = len(r)
+    q_cm = np.array([0.1, 1, 2])
+    beta = 0.5
+    strength = 5
+
+    psi_ell = yamaguchi_radial_wave_function(
+        r=r, q_cm=q_cm[:, None], beta=beta, strength=strength
+    )
+
+    # When it is inserted in the Schrodinger equation with a non-local Yamaguchi external potential
+    f_r = yamaguchi_form_factor_position_space(r=r, beta=beta, ell=ell)
+    V_r = f_r[:, None] * f_r * strength
+    residual = schrodinger_residual(
+        psi=psi_ell, V=V_r, r=r, dr=dr, q_cm=q_cm[:, None], ell=ell
+    )
+    # And endpoint effects are removed (due to numerical gradients)
+    residual = residual[..., 2:-2]
+
+    # Then the residual should be almost zero
+    np.testing.assert_allclose(
+        actual=residual, desired=np.zeros_like(residual), atol=1e-2, rtol=1
+    )
+
+
+def test_wave_function_from_reactance_matrix():
+    # Rule: The analytic Yamaguchi scattering amplitude should match the empirical version from the LS equation
+
+    # Given a quadrature mesh
+    n_intervals = 21
+    nodes = np.linspace(0, 10, n_intervals)
+    n_points = 100 * np.ones(n_intervals - 1, dtype=int)
+    mesh = CompoundMesh(nodes, n_points)
+    k, dk = mesh.x, mesh.w
+    r, dr = mesh.x, mesh.w
+
+    # And other parameters
+    beta = 2.0
+    ell = 0
+    q_cm = np.array([0.1, 1, 2])
+    strength = np.array([3])
+    hbarsq_over_M = 1
+
+    # And a Yamaguchi potential
+    f_k = yamaguchi_form_factor_momentum_space(k=k, beta=beta, ell=ell)
+    f_r = yamaguchi_form_factor_position_space(r=r, beta=beta, ell=ell)
+    V1_k = (f_k[:, None] * f_k)[..., None] * strength
+    V1_r = (f_r[:, None] * f_r)[..., None] * strength
+
+    # When the Lippmann Schwinger equation is evaluated to solve for the reactance matrix, K
+    newton = NewtonEmulator(
+        V0=np.zeros_like(V1_k[..., 0]),
+        V1=V1_k,
+        k=k,
+        dk=dk,
+        q_cm=q_cm,
+        boundary_condition=BoundaryCondition.STANDING,
+        nugget=1e-10,
+    )
+
+    schwing = KohnLippmannSchwingerEmulator(
+        V0=np.zeros_like(V1_r[..., 0]),
+        V1=V1_r,
+        r=r,
+        dr=dr,
+        NVP=newton,
+        inv_mass=hbarsq_over_M,
+        ell=ell,
+    )
+
+    # And the K matrix from the LS equation is converted to a wave function in position space
+    params = np.array([1])
+    psi_schwing = schwing.predict(params, full_space=True).T
+
+    # And the analytic Yamaguchi wave function is evaluated
+    psi_analytic = yamaguchi_radial_wave_function(
+        r=r, q_cm=q_cm[:, None], beta=beta, strength=strength
+    )
+
+    # Then the wave function from the LS equation and the analytic version should match
+    np.testing.assert_allclose(
+        actual=psi_schwing, desired=psi_analytic, atol=1e-5, rtol=1e-1
+    )
 
 
 def test_quadrature_infinite():
